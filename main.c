b6f36672f57f8defa572266dc17973e1ff7516bf
SerumYouNeed <greg.miler@gmail.com>
2026-03-13 19:51:30 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int integer;
+
+    integer = 5;
+    integer = integer + 10;
+    int integer2 = 4;
+    int sum;
+    sum = integer + integer2;
+    printf("%d\n", sum);
+    return 0;
+}
