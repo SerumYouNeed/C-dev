8c56207150be97ebd22a1cc7588890be00db7fa6
SerumYouNeed <greg.miler@gmail.com>
2026-03-17 20:45:09 +0100

    day 2

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 int main()
 {
     int integer;
+    float ulamek;
+    char napis[] = "To jest napis";
+
+    printf("To jest liczba: %d\n", integer);
+    printf("To jest ułamek: %f\n", ulamek);
+    printf("To jest ułamek z dokładnością: %.2f\n", ulamek);
+    printf("To jest napis: %s\n", napis);
+
 
     integer = 5;
     integer = integer + 10;
